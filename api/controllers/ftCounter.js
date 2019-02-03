2ad8f840248c8cf2a3cf67a30a47289e3efa97f9
Jan Henning <triton539@hotmail.com>
2019-02-03 04:23:44 +0100

    Fix typo

--- api/controllers/ftCounter.js
+++ api/controllers/ftCounter.js
@@ -1,28 +1,28 @@
-const FTCount = require("../../models/ftcount");
+const FTCount = require('../../models/ftCount');
 
 // GET /counters
 exports.counters = (req, res, next) => {
-  res.status(404).json({
-    error: {
-      message: "Missing server ID in URL - Please specify it like this: /ftCounter/<server_id>"
-    }
-  });
+	res.status(404).json({
+		error: {
+			message: 'Missing server ID in URL - Please specify it like this: /ftCounter/<server_id>'
+		}
+	});
 };
 
 // GET /counters/:serverId
 exports.counters_get_server = (req, res, next) => {
-  const serverId = req.params.serverId;
-  FTCount.find({ server_id: serverId })
-    .exec()
-    .then(doc => {
-      res.status(200).json({
-        ft_counter: doc
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
-    });
+	const serverId = req.params.serverId;
+	FTCount.find({ server_id: serverId })
+		.exec()
+		.then((doc) => {
+			res.status(200).json({
+				ft_counter: doc
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({
+				error: err
+			});
+		});
 };
