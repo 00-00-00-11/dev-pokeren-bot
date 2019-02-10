162245117c4a1a6043f6f505ed97d784046a6b27
Jan Henning <triton539@hotmail.com>
2019-02-10 23:05:16 +0100

    Uncomment a comment

--- modules/events/channelUpdate.js
+++ modules/events/channelUpdate.js
@@ -4,7 +4,7 @@ const mainServer = process.env.POKEREN_SERVER;
 module.exports = (bot, oldChannel, newChannel) => {
 	const g = bot.guilds.get(staffServer);
 	const ch = g.channels.get('543446264780554244');
-	// if (oldChannel.guild.id !== mainServer) return;
+	if (oldChannel.guild.id !== mainServer) return;
 
 	if (oldChannel.name !== newChannel.name) {
 		ch.send(`⚙️ Channel \`${oldChannel.name}\` (${oldChannel.id}) was renamed to \`${newChannel.name}\``);
